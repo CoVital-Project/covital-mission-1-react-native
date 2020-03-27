5640238167d07420a38cdea72324efef114d54ad
Areadrill <jackthemotorcycle@gmail.com>
2020-03-27 01:42:40 +0000

    chore: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+    },
+    "extends": [
+        "@moxy/eslint-config-base/esm",
+        "@moxy/eslint-config-babel",
+        "@moxy/eslint-config-react",
+        "@moxy/eslint-config-react-web-a11y",
+        "@moxy/eslint-config-jest"
+    ]
+}
